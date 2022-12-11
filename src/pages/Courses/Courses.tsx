acb61834fc706b1445676d59c102b7ea636f7e6d
tonycherednichenko <tony.cherednichenko95@yandex.ru>
2022-12-11 12:33:42 +0300

    add skeleton loading

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material"
 import { PageBuilder } from "components/organisms";
 import { useGetPage } from "hooks"
 import { Pages } from "__data__"
+import { Skelet } from "./Skelet";
 
 const Courses = () => {
   const { isLoading, data, error } = useGetPage(Pages.Courses)
@@ -9,7 +10,7 @@ const Courses = () => {
   return (
     <Box>
       {isLoading && (
-        <span>loading</span>
+        <Skelet />
       )}
       {!isLoading && (
         <PageBuilder features={data} error={error} />
---
+++ src/pages/Courses/Skelet.tsx
@@ -0,0 +1,33 @@
+import { Skeleton } from "@mui/material";
+import { Box } from "@mui/system";
+
+const Skelet = () => {
+  const cardList = [
+    { width: "550px", height: '350px', id: 1 },
+    { width: "550px", height: '350px', id: 2 }
+  ]
+  return (
+    <>
+      <Box sx={{
+        width: 'max-content',
+        display: 'flex',
+        gap: '15px',
+        flexDirection: 'column',
+        m: '0 auto',
+        py: '25px',
+      }}>
+        {cardList.map((card) => (
+          <Skeleton
+            variant="rectangular"
+            animation="wave" 
+            width={card.width} 
+            height={card.height} 
+            key={card.id} 
+          />
+        ))}
+      </Box>
+    </>
+  )
+}
+
+export { Skelet }
